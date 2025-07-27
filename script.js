34907f0c3b2b172714a94be9c2be477d581edb5d
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-27 22:33:50 +0200

    refactor: all. style: details.

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ function renderAllFruits() {
 async function getAllFruits() {
   let fruitsRef = document.getElementById("main-content-container");
   fruitsRef.innerHTML = "";
-  const proxy = "https://cors-anywhere.herokuapp.com/";
-  const url = "https://www.fruityvice.com/api/fruit/all";
-  const response = await fetch(proxy + url);
-  const fruits = await response.json();
-  for (let i = 0; i < fruits.length; i++) {
-    const fruit = fruits[i];
-    fruitsRef.innerHTML += fruitTemplate(fruit);
-    console.log(fruit);
+  try {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://www.fruityvice.com/api/fruit/all";
+    const response = await fetch(proxy + url);
+    const fruits = await response.json();
+
+    for (let i = 0; i < fruits.length; i++) {
+      const fruit = fruits[i];
+      fruitsRef.innerHTML += fruitTemplate(fruit);
+      console.log(fruit);
+    }
+  } catch (error) {
+    fruitsRef.innerHTML = errorTemplate();
   }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -4,30 +4,35 @@ function fruitTemplate(fruit) {
                 <h2>${fruit.name}</h2>
                 <table>
                     <tr>
-                        <td>Gattung</td>
+                        <td>Gattung:</td>
                         <td>${fruit.genus}</td>
                     </tr>
                     <tr>
-                        <td>Familie</td>
+                        <td>Familie:</td>
                         <td>${fruit.family}</td>
                     </tr>
                     <tr>
-                        <td>Protein</td>
+                        <td>Protein:</td>
                         <td>${fruit.nutritions.protein}g</td>
                     </tr>
                     <tr>
-                        <td>Kohlenhydrate</td>
+                        <td>Kohlenhydrate:</td>
                         <td>${fruit.nutritions.carbohydrates}g</td>
                     </tr>
                     <tr>
-                        <td>kalorien</td>
+                        <td>Kalorien:</td>
                         <td>${fruit.nutritions.calories}Kcal</td>
                     </tr>
                     <tr>
-                        <td>Zucker</td>
+                        <td>Zucker:</td>
                         <td>${fruit.nutritions.sugar}g</td>
                     </tr>
                 </table>
             </div>
          `;
+}
+
+function errorTemplate() {
+  return `
+            <p>Fehler bei laden der Früchte!`;
 }
