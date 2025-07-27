6aafd18d959c005c859bf1a8cb4a072e2e56e2d6
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-27 20:44:47 +0200

    sript.js: renderAllFruits() and getAllFruits() added

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function renderAllFruits() {
   let fruitsRef = document.getElementById("main-content-container");
   fruitsRef.innerHTML = "";
 }
+
+async function getAllFruits() {
+  const response = await fetch("https://www.fruityvice.com/api/fruit/all");
+  const fruits = await response.json();
+  for (let fruitIndex = 0; fruitIndex < fruits.length; fruitIndex++) {
+    const fruit = fruits[fruitIndex];
+  }
+}
